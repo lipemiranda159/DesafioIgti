01dcbd302aeb6500cc1f050c2b0445c6c2a5a5be
lipemiranda159 <lipemiranda159@hotmail.com>
2020-07-10 02:05:58 -0300

    create dbService structure

--- app/services/dbService.ts
+++ app/services/dbService.ts
@@ -22,4 +22,22 @@ class DbService {
       throw error;
     }
   };
+
+  getAllTransactions = async (filter: any | undefined) => {
+    try {
+      return filter
+        ? await this.dbContext.find({ filter })
+        : await this.dbContext.find({});
+    } catch (error) {
+      return error;
+    }
+  };
+
+  deleteAccount = async (filter: any) => {
+    await this.dbContext.deleteOne(filter);
+  };
+
+  save = async (obj: any) => {
+    this.dbContext.save(obj);
+  };
 }
