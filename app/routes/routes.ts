16c296917d3e4cd1395270553bce008b807a317f
lipemiranda159 <lipemiranda159@hotmail.com>
2020-07-13 01:47:33 -0300

    use login validation

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import Login from "../controllers/usersController";
 const transactionRouter = express.Router();
+const login = new Login();
 
 /**
  * Rota raiz
  */
-transactionRouter.get("/api/", (_, response) => {
+transactionRouter.get("/api/", login.verifyJWT, (_, response) => {
   response.send({
     message:
       "Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações",
