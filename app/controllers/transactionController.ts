d5f3d37b6613232ea6f86fbf7b2fc24932778a4d
lipemiranda159 <lipemiranda159@hotmail.com>
2020-07-13 00:12:03 -0300

    add transaction controller

---
+++ app/controllers/transactionController.ts
@@ -0,0 +1,57 @@
+import { Request, Response, request } from "express";
+import TransactionService from "../services/transactionService";
+
+class TransactionController {
+  getErrorCode(e: any) {
+    return 500;
+  }
+  private accountService: TransactionService;
+  private NoFilterTransaction = undefined;
+  constructor() {
+    this.accountService = new TransactionService();
+  }
+
+  getTransactions = async (request: Request, response: Response) => {
+    try {
+      const { take, order } = request.query;
+      let result = await this.accountService.GetAllTransactions(
+        this.NoFilterTransaction
+      );
+      response.send(result);
+    } catch (e) {
+      const codeError = this.getErrorCode(e);
+      response.status(codeError).send({ res: `${e}` });
+    }
+  };
+
+  getTransaction = async (request: Request, response: Response) => {
+    try {
+      const result = await this.accountService.getTrasaction();
+      response.send(result);
+    } catch (e) {
+      const codeError = this.getErrorCode(e);
+      response.status(codeError).send({ res: `${e}` });
+    }
+  };
+
+  addTransaction = async (request: Request, response: Response) => {
+    try {
+      this.accountService.addTransaction();
+      response.send("ok");
+    } catch (e) {
+      const codeError = this.getErrorCode(e);
+      response.status(codeError).send({ res: `${e}` });
+    }
+  };
+
+  deleteTransaction = async (request: Request, response: Response) => {
+    try {
+      response.send(await this.accountService.deleteTransaction());
+    } catch (e) {
+      const codeError = this.getErrorCode(e);
+      response.status(codeError).send({ res: `${e}` });
+    }
+  };
+}
+
+export default TransactionController;
--- app/services/transactionService.ts
+++ app/services/transactionService.ts
@@ -1,1 +1,7 @@
-class TransactionService {}
+class TransactionService {
+  GetAllTransactions = async (NoFilterBalance: any) => {
+    throw new Error("Method not implemented.");
+  };
+}
+
+export default TransactionService;
